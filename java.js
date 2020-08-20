19fbd96f3cb3b0e5e015d86e043f732e2f0ba396
Ahmad Alhashimi <ahmad.alhashimi83@gmail.com>
2020-08-20 15:47:27 +0200

    04-08-2020

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
+//05082020
 console.log(2)
+var arr = (['aa',33,'ahmed' ,4])
+console.log(arr[2])
+console.log({
+    'name': 'ahmad',
+    'hobbies': ['a', 'b', ['aaa','eee'], true, 7],
+    'age': true
+})
+console.log(typeof(arr[0]), typeof(arr[1]))
+console.log(arr.length)
+console.log(arr[2])
