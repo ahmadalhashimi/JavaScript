1de5da7913b08a115f4bf5edef5de9aa61b27cc6
Ahmad Alhashimi <ahmad.alhashimi83@gmail.com>
2020-08-21 18:51:11 +0200

    Array

---
+++ S_6th_August.js
@@ -0,0 +1,22 @@
+var my_array = ['bmw', 3, true, 10]
+// console.log(my_array)
+// console.log(my_array[0]) 
+// console.log(my_array[1]) 
+// console.log(my_array[2]) 
+// console.log(my_array[3]) 
+
+// for(i=0; i<my_array.length; i++){
+//     console.log(my_array[i])
+// }
+for(i=1; i<=my_array.length; i++){
+    console.log(my_array[i])
+}
+for(i=1; i<=my_array.length; i++){
+    console.log(my_array[i-1])
+}
+
+var i =0
+while(i< my_array.length){
+    console.log(my_array[i])
+    i++
+}
---
+++ ThirdNumInAray.js
@@ -0,0 +1,12 @@
+function thirdNumber(array){
+    var first = array[0]
+    var second = array[0]
+    var third = array [0]
+    for ( var i=0 i<array.lenth; i++){
+        if(array[i]> first){
+            first = array[i]
+            second = first
+            third =
+                }
+    }
+}
